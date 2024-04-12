551895329a3ef4e863c93356dc1757f866d23006
Harry MacCode <emarh.harry.code@gmail.com>
2024-04-12 19:46:03 +0200

    User Model

---
+++ models/user.model.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+
+// Schema
+userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    trialService: {
+        type: Boolean,
+        default: false,
+    },
+    trialExpires: {
+        type: Date
+    },
+    subscription: {
+        type: String,
+        enum: ['Trial', 'Free', 'Basic', 'Premium']
+    },
+    apiRequestCount: {
+        type: Number,
+        default: 0
+    },
+    monthlyRequestCount: {
+        type: Number,
+        default: 0
+    },
+    nextBillingDate: Date,
+    payments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Payment',
+        }
+    ],
+    history: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'History'
+        }
+    ]
+}, {
+    timeseries: true
+});
+
+//! Compile to form the model
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
