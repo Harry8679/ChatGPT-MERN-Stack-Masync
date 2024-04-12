877df9ca52907b514b6352bbd8fa54b2f1410ce1
Harry MacCode <emarh.harry.code@gmail.com>
2024-04-12 20:27:24 +0200

    Register Controller Part 1

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const userRouter = require('./routes/user.route');
 dotenv.config();
 
-const PORT = process.env.PORT || 8000;
+
+const PORT = process.env.PORT || 9000;
+
+// ----- Routes -----
+app.use('/api/v1/users', userRouter);
 
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
